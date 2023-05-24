7e346047abcfce669312647f4c19adc0ec549748
Comrade Yi <ambiguous-pointer@outlook.com>
2023-05-24 15:49:59 +0800

    initi

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.FutureTask;
+
+public class App {
+    public static void main(String[] args) {
+
+    }
+}
